6e1040fc3bef8e146b4f899aad1b7a7da8c5e12a
Harshavardhan Reddy Kundavaram <harshavardhanreddyk@uber.com>
2020-05-25 10:47:06 +0530

    circularArrayRotation

--- HackerRank/circularArrayRotation/circularArrayRotation.go
+++ HackerRank/circularArrayRotation/circularArrayRotation.go
@@ -12,19 +12,19 @@ import (
 
 // Complete the circularArrayRotation function below.
 func circularArrayRotation(a []int32, k int32, queries []int32) []int32 {
-	var opArray []int32
+	log.Println("circularArrayRotation")
+	var opArray, tempVal []int32
 	length := int32(len(a))
-	var indx, i int32
-	//k rotations
-	log.Println("Provided Array => ", a)
-	for indx < k {
-		tempVal := a[length-1]
-		a = a[0 : length-1]
-		a = append([]int32{tempVal}, a...)
-		log.Println("newArray => ", a)
-		indx++
+	var i int32
+	if k/length > 0 {
+		k = k % length
 	}
-	log.Println("queries ", queries)
+	//k rotations
+	if k > 0 {
+		tempVal = a[length-k:]
+		log.Println("tempVal => ", tempVal)
+		a = append(tempVal, a...)
+	}
 	for i < int32(len(queries)) {
 		opArray = append(opArray, a[queries[i]])
 		i++
