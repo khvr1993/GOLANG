6aac45ee05a0d9ede95a17bbe7e3e5046335bdd6
Harshavardhan Reddy Kundavaram <harshavardhanreddyk@uber.com>
2020-05-29 06:13:10 +0530

    nonDivisibleSubset

---
+++ HackerRank/nonDivisibleSubset/nonDivisibleSubset.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+func nonDivisibleSubset(k int32, s []int32) int32 {
+	// Write your code here
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	sTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var s []int32
+
+	for i := 0; i < int(n); i++ {
+		sItemTemp, err := strconv.ParseInt(sTemp[i], 10, 64)
+		checkError(err)
+		sItem := int32(sItemTemp)
+		s = append(s, sItem)
+	}
+
+	result := nonDivisibleSubset(k, s)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
