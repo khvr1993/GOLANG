b3d7d9605a0a184513d256a9b086d04176767503
Harshavardhan Reddy Kundavaram <harshavardhanreddyk@uber.com>
2021-05-01 16:04:58 +0530

    two Sum II

---
+++ leetcode/twoSumII/twoSumII/twoSumII.go
@@ -0,0 +1,23 @@
+package twoSum
+
+import "fmt"
+
+func twoSum(numbers []int, target int) []int {
+	left := 0
+	right := len(numbers) - 1
+	currentSum := 0
+	fmt.Println(left, right)
+
+	for left < right {
+		currentSum = numbers[left] + numbers[right]
+		if currentSum == target {
+			return []int{left + 1, right + 1}
+		} else if currentSum > target {
+			//move the right pointer
+			right -= 1
+		} else {
+			left += 1
+		}
+	}
+	return nil
+}
